78130c46994572fbaa27a1dcf7f4204e9b586d04
Kurachi <naist1020@gmail.com>
2019-07-08 11:49:37 +0900

    edit

--- src/ast_analyze_executor.py
+++ src/ast_analyze_executor.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
 
     # target_file_path = 'C:/pbl/home/junit/src/test/java/jp/enpit/cloud/junit/CalculatorTest.java'
     target_file_path = 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven/maven-model-builder/src/test/java/org/apache/maven/model/interpolation/StringSearchModelInterpolatorTest.java'
+    # target_file_path = 'C:/Users/ryosuke-ku/Desktop/NiCad-5.1/systems/maven/maven-model-builder/src/main/java/org/apache/maven/model/interpolation/StringSearchModelInterpolator.java'
 
     # ★ポイント１
     ast_info = AstProcessor(None, BasicInfoListener()).execute(target_file_path)
